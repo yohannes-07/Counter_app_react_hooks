17c701c76a772a874b97453481b36cf517a1dd35
Yohannes Teffera <yohannesahunm7@gmail.com>
2023-08-04 12:39:43 +0300

    React Counter app to demonstrate react hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useRef} from 'react';
+import './App.css'
 
 function App() {
+  // counter state variable to be updated when increment and decrement buttons are clicked
+  const [counter, setCounter] = useState<number>(0)
+
+  // To reference buttons elements 
+  const incrementRef = useRef<HTMLButtonElement>(null)
+  const decrementRef = useRef<HTMLButtonElement>(null)
+
+
+  // This effect updates the document title when the counter dependency changes
+  useEffect(() => {
+    document.title = `Counter value => ${counter}`
+    
+  }, [counter])
+
+  // function to handle increment the counter 
+  // focus after increment the counter
+  const handleIncrement = ():void  => {
+    setCounter(c => c + 1)
+    incrementRef.current?.focus()
+  }
+  
+  // function to handle increment the counter 
+  // focus after decrement the counter
+  const handleDecrement = ():void  => {
+    setCounter(c => c - 1)
+    incrementRef.current?.focus()
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <h1 className='counter' >{counter}</h1>
+      <div className="buttons">
+
+        <button onClick={handleIncrement} ref={incrementRef} className="btn">Increment</button>
+        <button onClick={handleDecrement} ref={decrementRef} className="btn">Decrement</button>
+
+      </div>
+      
     </div>
   );
 }
